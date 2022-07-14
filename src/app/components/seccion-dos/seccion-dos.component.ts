d24c84475992cc1d57c8a659d09da9a821d0e087
Santiago Barrera <santiago.barrerae@gmail.com>
2022-07-14 00:18:40 -0500

    nuevas secciones

---
+++ src/app/components/seccion-dos/seccion-dos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seccion-dos',
+  templateUrl: './seccion-dos.component.html',
+  styleUrls: ['./seccion-dos.component.scss']
+})
+export class SeccionDosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
